4a7036496a7dfb385325e44164dac1cd4a3192c2
ohmoka <projectfrags@gmail.com>
2021-11-15 15:58:40 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+#clean_downloads.py
+
+A small python script to clean your downloads folder. 
+
+##Dependencies
+This script has one dependency, being the python module send2trash.
+It can be installed via the following command:
+
+```
+  pip install send2trash
+```
+
+##Setup:
+
+1. Open clean_downloads.py
+2. Change the 'PATH' global string variable to ***your*** absolute downloads directory.
+
+##Configuration:
+
+* The `LIST_EXTENSIONS` global variable can be appended with whatever file extensions you would like to filter through and delete.
+* By default, clean_downloads.py looks for .zip, .rar, .7z, .png, and .jpg files.
+
+##Execute:
+```
+  python3 clean_downloads.py
+```
---
+++ clean_downloads.py
@@ -0,0 +1,55 @@
+import os
+import time
+from send2trash import send2trash
+
+PATH = 'C:\\Users\\proje\\Downloads'
+DOWNLOADS_DIR = os.chdir(PATH)
+DOWNLOADS_LIST = os.listdir(DOWNLOADS_DIR)
+LIST_EXTENSIONS = ['.zip', '.rar', '.7z', '.jpg', '.png']
+EXTENSIONS = tuple(LIST_EXTENSIONS)
+
+
+def get_removed_list(_downloads_list):
+    return [dl for dl in _downloads_list if dl.endswith(EXTENSIONS)]
+
+
+def get_removed_paths(_removed_list):
+    _removed_paths = [os.path.join(PATH, i) for i in _removed_list]
+    return _removed_paths
+
+
+def main():
+    print('Listing files to be removed: \n')
+    time.sleep(1)
+
+    removed_list = get_removed_list(DOWNLOADS_LIST)
+    number_of_removed = len(removed_list)
+    removed_paths = get_removed_paths(removed_list)
+
+    for i in removed_list:
+        print(i)
+
+    if number_of_removed == 0:
+        print('Nothing to remove!')
+        time.sleep(2)
+        exit()
+    else:
+        print('\nNumber of files to be removed is:', number_of_removed)
+        time.sleep(2)
+
+    while True:
+        choice = input('\nWould you like to remove these files? y/n: ')
+
+        if choice == 'y' or choice == 'Y':
+            print('Sending', number_of_removed, 'file(s) to the trash!')
+            send2trash(removed_paths)
+            break
+
+        if choice == 'n' or choice == 'N':
+            print('Exiting now...')
+            time.sleep(1)
+            break
+
+
+if __name__ == '__main__':
+    main()
